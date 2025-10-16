7f959477cb19ec70cf7eebb8332cadae0fba53f7
Sudeep Chowdhury <sudeep@itobuz.com>
2025-10-16 11:42:23 +0530

    this is the last commit.

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,110 @@
+import { useContext, useActionState } from "react";
+import Modal from "./UI/Modal.jsx";
+import CartContext from "../store/CartContext.jsx";
+import { currencyFormatter } from "../util/formatting.js";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import useHttp from "../hooks/useHttp.js";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const { data, error, sendRequest, clearData } = useHttp(
+    "http://localhost:3000/orders",
+    requestConfig
+  );
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleFinish() {
+    userProgressCtx.hideCheckout();
+    cartCtx.clearCart();
+    clearData();
+  }
+
+  async function checkoutAction(prevState, fd) {
+    const customerData = Object.fromEntries(fd.entries());
+
+    await sendRequest(
+      JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      })
+    );
+  }
+
+  const [formState, formAction, isSending] = useActionState(
+    checkoutAction,
+    null
+  );
+
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={handleClose}>
+        Close
+      </Button>
+      <Button>Submit Order</Button>
+    </>
+  );
+
+  if (isSending) {
+    actions = <p>Sending order data ...</p>;
+  }
+
+  if (data && !error) {
+    return (
+      <Modal
+        open={userProgressCtx.progress === "checkout"}
+        onClose={handleFinish}
+      >
+        <h2>Order Successful!</h2>
+        <p>Thank you for your order!</p>
+        <p>
+          We will get back to with more details via email within the next few
+          minutes.
+        </p>
+        <p className="modal-actions">
+          <Button onClick={handleFinish}>Okay</Button>
+        </p>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form action={formAction}>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        {error && <Error title="Failed to submit order!" message={error} />}
+
+        <p className="modal-actions">{actions}</p>
+      </form>
+    </Modal>
+  );
+}
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ children, open, onClose, className = "" }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    const modal = dialog.current;
+
+    if (open) {
+      modal.showModal();
+    }
+
+    // This is a clean up function that closes the modal when the component unmounts or when `open` changes to false
+    return () => modal.close();
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
